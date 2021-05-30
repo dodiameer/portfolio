43249e89305c8f1c1a60aa3e3cc056caac19bb9b
dodiameer <mtxshiftg@gmail.com>
2021-05-30 18:30:18 +0300

    responsive: Navbar

--- src/lib/components/routes/__layout/Navbar.svelte
+++ src/lib/components/routes/__layout/Navbar.svelte
@@ -9,8 +9,11 @@
 <template>
   <div class="nav-wrapper">
       <nav class="nav container">
-        <a href="/home" class="nav__link" class:nav__link--active={currentRoute === "home"}>home</a>
-        <a href="/about" class="nav__link" class:nav__link--active={currentRoute === "about"}>about</a>
+        <p class="nav__brand">dodiameer</p>
+        <div class="nav__links">
+          <a href="/home" class="nav__link" class:nav__link--active={currentRoute === "home"}>home</a>
+          <a href="/about" class="nav__link" class:nav__link--active={currentRoute === "about"}>about</a>
+        </div>
       </nav>
   </div>
 </template>
@@ -21,13 +24,32 @@
     color: black;
   }
   .nav {
-    padding: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: center;
-    @include flow-x(1rem);
+    justify-content: space-between;
+    @include media("<tablet") {
+      @include flow-y;
+      flex-direction: column;
+    }
+    
+    .seperator {
+      flex: 1;
+    }
+    
+    &__brand {
+      font-size: 1.5rem;
+    }
 
+    &__links {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      @include flow-x(1rem);
+    }
+    
     &__link {
       color: inherit;
       text-decoration: none;
