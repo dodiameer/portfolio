067ff66e7944b4007561f6cced12d2f206c232ec
dodiameer <mtxshiftg@gmail.com>
2021-05-30 18:21:39 +0300

    component: Navbar

---
+++ src/lib/components/routes/__layout/Navbar.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+  let currentRoute: string
+  $: currentRoute = "/home"
+</script>
+
+<template>
+  <div class="nav-wrapper">
+      <nav class="nav container">
+        <a href="/home" class="nav__link" class:nav__link--active={currentRoute === "/home"}>home</a>
+        <a href="/about" class="nav__link" class:nav__link--active={currentRoute === "/about"}>about</a>
+      </nav>
+  </div>
+</template>
+
+<style lang='scss'>
+  .nav-wrapper {
+    background: white;
+    color: black;
+  }
+  .nav {
+    padding: 1rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    @include flow-x(1rem);
+
+    &__link {
+      color: inherit;
+      text-decoration: none;
+
+      &--active {
+        color: get-color("primary");
+      }
+    }
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,7 @@
 <script lang='ts'>
   import "$lib/sass/global.scss"
+  import Navbar from "$lib/components/routes/__layout/Navbar.svelte";
 </script>
 
+<Navbar />
 <slot />
