d2c46afbcb43b93a69a135a317237147fb8f4764
dodiameer <mtxshiftg@gmail.com>
2021-05-30 18:56:55 +0300

    chore: Button mixin

---
+++ src/lib/components/routes/home/HeroSection.svelte
@@ -0,0 +1,7 @@
+<template>
+  <section class="hero">
+    <h1 class="hero__heading">I build attractive sites that help you sell</h1>
+    <h2 class="hero__subheading">Frontend Developer in İstanbul</h2>
+    <button class="btn">Get in touch</button>
+  </section>
+</template>
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+import HeroSection from "$lib/components/routes/home/HeroSection.svelte";
+</script>
+<HeroSection />
 <div class="container">
   <h1>dodiameer</h1>
 </div>
