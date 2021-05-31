9a7d4a8ae662feaeebda8d067b4ff2ecdc5707de
dodiameer <mtxshiftg@gmail.com>
2021-05-31 18:04:50 +0300

    wip: Work section

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -2,12 +2,14 @@
 import HeroSection from "$lib/components/routes/home/HeroSection.svelte";
 import HomeNavbar from "$lib/components/routes/home/HomeNavbar.svelte";
 import SkillsSection from "$lib/components/routes/home/SkillsSection.svelte";
+import WorkSection from "$lib/components/routes/home/WorkSection.svelte";
 </script>
 
 <HeroSection />
 <HomeNavbar />
 <div class="container">
   <SkillsSection />
+  <WorkSection />
 </div>
 
 
