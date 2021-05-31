f3ab28f4a5473c94097e0211abe7815dfc3a9420
dodiameer <mtxshiftg@gmail.com>
2021-05-31 17:55:49 +0300

    page/home: Changed skill section icons

--- src/lib/components/routes/home/SkillsSection.svelte
+++ src/lib/components/routes/home/SkillsSection.svelte
@@ -1,19 +1,22 @@
 <script>
 import Card from "$lib/components/Card.svelte";
-import LayoutIcon from "$lib/components/icons/LayoutIcon.svelte";
+import HtmlIcon from "$lib/components/icons/HTMLIcon.svelte";
+import JavascriptIcon from "$lib/components/icons/JavascriptIcon.svelte";
+import SassIcon from "$lib/components/icons/SassIcon.svelte";
+import SvelteIcon from "$lib/components/icons/SvelteIcon.svelte";
 
 </script>
 <template>
   <section class="row skills" id="skills-section">
     <h2 class="col-md-12 skills__title">Skills</h2>
     <Card title="HTML & CSS" class="col-md-6 col-lg-12">
-      <LayoutIcon slot="icon" width="48" />
+      <HtmlIcon slot="icon" width="48" />
       The two most fundamental skills in web development. 
       Additionally, I use semantic elements which improve 
       accessibility for disabled people. 
     </Card>
     <Card title="Javascript & Typescript" class="col-md-6 col-lg-4">
-      <LayoutIcon slot="icon" width="48" />
+      <JavascriptIcon slot="icon" width="48" />
       The Javascript programming language that powers virtually
       all websites, allows you to add rich interactions and 
       mobile-app like features to your site. Typescript is a 
@@ -21,14 +24,14 @@ import LayoutIcon from "$lib/components/icons/LayoutIcon.svelte";
       easier-to-understand code
     </Card>
     <Card title="Sass" class="col-md-6 col-lg-4">
-      <LayoutIcon slot="icon" width="48" />
+      <SassIcon slot="icon" width="48" />
       Sass is an extension to CSS, which lets you write code 
       quicker, as well as break down code into tiny, maintainable,
       reusable chunks, all of which can get your website done quicker
       and much more cleanly. 
     </Card>
     <Card title="Svelte" class="col-md-6 col-lg-4">
-      <LayoutIcon slot="icon" width="48" />
+      <SvelteIcon slot="icon" width="48" />
       A framework that breaks down your website into small chunks called
       components, it differs from other frameworks like React and Vue by
       doing most it's work when building the website, which makes your site
