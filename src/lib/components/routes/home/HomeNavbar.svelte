52278cc1840e087097e5e48e0b2c86336734a32c
dodiameer <mtxshiftg@gmail.com>
2021-05-31 17:33:47 +0300

    feat: Added quick navigation to homepage

---
+++ src/lib/components/routes/home/HomeNavbar.svelte
@@ -0,0 +1,39 @@
+<template>
+  <div class="nav-wrapper">
+    <h2 class="nav-wrapper__title">Quick navigation</h2>
+    <nav class="nav container">
+      <a href="#skills-section" class="nav__link btn-secondary">Skills</a>
+    </nav>
+  </div>
+</template>
+
+<style lang='scss'>
+  .nav-wrapper {
+    @include flow-y(1rem); 
+    padding: 1rem 0.66rem;
+    background: get-color("primary");
+    color: get-color("primary", "content");
+
+    &__title {
+      font-size: 1.2rem;
+      font-weight: normal;
+      text-align: center;
+      color: inherit;
+    } 
+  } 
+  .nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &__link {
+      // font-size: 0.85rem;
+      // padding-top: 0.5rem;
+      // padding-bottom: 0.5rem;
+      text-decoration: none;
+      min-width: 10ch;
+      text-align: center;
+      // font-weight: bold
+    }
+  }  
+</style>
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 import HeroSection from "$lib/components/routes/home/HeroSection.svelte";
+import HomeNavbar from "$lib/components/routes/home/HomeNavbar.svelte";
 import SkillsSection from "$lib/components/routes/home/SkillsSection.svelte";
 </script>
+
 <HeroSection />
+<HomeNavbar />
 <div class="container">
   <SkillsSection />
 </div>
