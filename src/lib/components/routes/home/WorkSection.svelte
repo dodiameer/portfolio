32de076889248b61d7976f75173b77545053cea3
dodiameer <mtxshiftg@gmail.com>
2021-06-01 20:04:45 +0300

    page/home: Finished work section

--- src/lib/components/routes/home/WorkSection.svelte
+++ src/lib/components/routes/home/WorkSection.svelte
@@ -5,24 +5,32 @@
 <template>
   <section class="row work" id="work-section">
     <h2 class="col-md-12 section-title">My work</h2>
-    <Card title="Portfolio" class="col-md-12 col-lg-6" image="/images/work/portfolio.png" imageAlt="Portfolio website screenshot">
-      Completely build from scratch, it uses a new 
-      framework called SvelteKit, made by the Svelte
-      team, which builds on top of Svelte to provide
-      features like server-side rendering (SSR), Static
-      generation (SSG), and client-side routing, all 
-      of which help boost your site's Search Engine 
-      Optimization (SEO) rating, making your site appear
-      at the top of search results, and improve the user 
-      experience when using your site.
+    <Card
+      title="Portfolio"
+      class="col-md-12 col-lg-6"
+      image="/images/work/portfolio.png"
+      imageAlt="Portfolio website screenshot">
+      Completely build from scratch, it uses a new framework called SvelteKit,
+      made by the Svelte team, which builds on top of Svelte to provide features
+      like server-side rendering (SSR), Static generation (SSG), and client-side
+      routing, all of which help boost your site's Search Engine Optimization
+      (SEO) rating, making your site appear at the top of search results, and
+      improve the user experience when using your site.
       <svelte:fragment slot="actions">
         <a href="#work-section" class="btn">Learn more</a>
       </svelte:fragment>
     </Card>
-    <Card title="Portfolio" class="col-md-12 col-lg-6" image="https://via.placeholder.com/800x600" imageAlt="Placeholder">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente iste quasi cupiditate voluptatem minima, in, architecto quia voluptate repellat ab doloribus ad temporibus a! Voluptatem consequatur reiciendis ullam incidunt aliquid!
+    <Card
+      title="Your website?"
+      class="col-md-12 col-lg-6"
+      image="/images/work/your-website.png"
+      imageAlt="Your future website's screenshot">
+      If you like my work, You can contact me to inquire about building your
+      website! The button below takes you straight to the contact form, which
+      has all my information, so you can contact me however you like. Looking
+      forward to hearing from you!
       <svelte:fragment slot="actions">
-        <a href="#work-section" class="btn">Learn more</a>
+        <a href="/contact" class="btn">Contact me</a>
       </svelte:fragment>
     </Card>
   </section>
