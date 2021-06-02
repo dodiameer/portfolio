3f46bd2cd76ac0ae5e20cc1f6f61b6e2d5142301
Mohammed Ali Agha <61318540+dodiameer@users.noreply.github.com>
2021-06-02 21:23:17 +0300

    general/ux (#5)

---
+++ src/lib/components/routes/__layout/BackToTop.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import ArrowUpIcon from "$lib/components/icons/ArrowUpIcon.svelte";
+  import { onMount } from "svelte";
+  type ButtonClickEvent = MouseEvent & {
+    currentTarget: EventTarget & HTMLButtonElement
+  };
+
+  let visible = false;
+
+  const goToTop = (e: ButtonClickEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    window.scrollTo({ behavior: "smooth", top: 0, left: 0 });
+    e.currentTarget.blur();
+  };
+
+  const scrollListener = () => {
+    const root = document.documentElement;
+    const scrollTotal = root.scrollHeight - root.clientHeight;
+    visible = root.scrollTop / scrollTotal > 0.20;
+  };
+
+  onMount(() => {
+    document.addEventListener("scroll", scrollListener);
+    return () => {
+      document.removeEventListener("scroll", scrollListener);
+    };
+  });
+</script>
+
+<button
+  class="btn btn-secondary"
+  class:btn--visible="{visible}"
+  aria-hidden="{!visible}"
+  on:click="{goToTop}"
+  aria-label="Go to the top of the page">
+  <ArrowUpIcon />
+</button>
+
+<style lang="scss">
+  .btn {
+    @include elevation(4);
+    border-radius: 9999px;
+    padding: 0;
+    $width: 3rem;
+    width: $width;
+    height: $width;
+    position: fixed;
+    $position: 0.75rem;
+    bottom: $position;
+    right: $position;
+    z-index: 100;
+    opacity: 0;
+    transform: translateY(100px);
+    transition: all 0.5s ease-out;
+
+    &--visible {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+</style>
