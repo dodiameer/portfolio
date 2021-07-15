0957aa3633d08abba491b5c89f79830906f94144
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-07-15 11:36:36 +0300

    page(home): Added additional skills

---
+++ src/lib/components/icons/ElixirIcon.svelte
@@ -0,0 +1,18 @@
+
+<script lang="ts">
+  export let width: string | number = 24;
+</script>
+
+<svg
+  role="img"
+  aria-label="Elixir Icon"
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  width="{width}"
+  height="{width}"
+  xmlns="http://www.w3.org/2000/svg"
+  ><title>Elixir</title>
+  <path d="M19.793 16.575c0 3.752-2.927 7.426-7.743 7.426-5.249 0-7.843-3.71-7.843-8.29 0-5.21 3.892-12.952 8-15.647a.397.397 0 0 1 .61.371 9.716 9.716 0 0 0 1.694 6.518c.522.795 1.092 1.478 1.763 2.352.94 1.227 1.637 1.906 2.644 3.842l.015.028a7.107 7.107 0 0 1 .86 3.4z"/>
+</svg>
+
+
