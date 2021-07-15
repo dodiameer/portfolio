6975ffb6bbfcb363fca9c5f36641cedfd3dc1ecc
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-07-15 11:47:09 +0300

    page(home): Added hover effect for skills section

--- src/lib/components/routes/home/SkillsSection.svelte
+++ src/lib/components/routes/home/SkillsSection.svelte
@@ -11,39 +11,39 @@ import NetlifyIcon from "$lib/components/icons/NetlifyIcon.svelte";
 <template>
   <section class="row skills" id="skills-section">
     <h2 class="col-md-12 section-title">Skills</h2>
-    <Card title="HTML & CSS" class="col-md-6 col-lg-12">
+    <Card title="HTML & CSS" class="col-md-6 col-lg-12" hoverEffect>
       <HtmlIcon slot="icon" width="48" />
       The two most fundamental skills in web development. Additionally, I use semantic
       elements which improve accessibility for disabled people.
     </Card>
-    <Card title="Javascript & Typescript" class="col-md-6 col-lg-4">
+    <Card title="Javascript & Typescript" class="col-md-6 col-lg-4" hoverEffect>
       <JavascriptIcon slot="icon" width="48" />
       The Javascript programming language that powers virtually all websites, allows
       you to add rich interactions and mobile-app like features to your site. Typescript
       is a superset of Javascript which help coders write cleaner, easier-to-understand
       code
     </Card>
-    <Card title="Sass" class="col-md-6 col-lg-4">
+    <Card title="Sass" class="col-md-6 col-lg-4" hoverEffect>
       <SassIcon slot="icon" width="48" />
       Sass is an extension to CSS, which lets you write code quicker, as well as
       break down code into tiny, maintainable, reusable chunks, all of which can
       get your website done quicker and much more cleanly.
     </Card>
-    <Card title="Elixir" class="col-md-6 col-lg-4">
+    <Card title="Elixir" class="col-md-6 col-lg-4" hoverEffect>
       <ElixirIcon slot="icon" width="48" />
       A functional programming language built on top of Erlang, used largely for distributed,
       fault-tolerant, and scalable applications. Combined with Phoenix, a web development
       framework, I use it for backend development, which allows me to get setup and
       write clean code very quickly.
     </Card>
-    <Card title="Svelte" class="col-md-6">
+    <Card title="Svelte" class="col-md-6" hoverEffect>
       <SvelteIcon slot="icon" width="48" />
       A framework that breaks down your website into small chunks called components,
       it differs from other frameworks like React and Vue by doing most it's work
       when building the website, which makes your site smaller in size and more performant
       when compared with others.
     </Card>
-    <Card title="Netlify" class="col-md-6">
+    <Card title="Netlify" class="col-md-6" hoverEffect>
       <NetlifyIcon slot="icon" width="48" />
       A web development platform and cloud hosting provider, it's the main hosting provider
       I use. They provide one of the fastest CDNs around the world, serverless functions,
