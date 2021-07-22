d7538d9622e6d459d3590aeb22fed491fbf3a47f
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-07-22 15:29:54 +0300

    deploy: Use Netlify. (Fuck you Vercel)

--- svelte.config.js
+++ svelte.config.js
@@ -25,7 +25,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: adapter({
+			// Output to "dist" folder
+			pages: "dist",
+			assets: "dist"
+		})
 	},
 };
 
