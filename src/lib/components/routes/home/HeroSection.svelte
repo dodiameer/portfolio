b6ef739fc7c3c241b6721108fabfbc66ef38c8ba
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 19:37:23 +0300

    refactor: Hero Section into plain CSS

--- src/lib/components/routes/home/HeroSection.svelte
+++ src/lib/components/routes/home/HeroSection.svelte
@@ -1,43 +1,50 @@
 <template>
-  <section class="hero" id="hero-section">
+  <section class="hero flow-y" id="hero-section">
     <h1 class="hero__heading">I build attractive sites that help you sell</h1>
     <h2 class="hero__subheading">Frontend Developer in İstanbul</h2>
     <button class="btn">Get in touch</button>
   </section>
 </template>
 
-<style lang='scss'>
+<style lang="postcss">
   .hero {
     min-height: 400px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    $overlay-color: rgba(0, 0, 0, 0.6);
-    background: linear-gradient(0deg, $overlay-color, $overlay-color), url("/hero-image.jpg"), get-color("primary");
+    --overlay-color: rgba(0, 0, 0, 0.6);
+    background: linear-gradient(
+        0deg,
+        var(--overlay-color),
+        var(--overlay-color)
+      ),
+      url("/hero-image.jpg"), var(--colors-primary-normal);
     background-repeat: no-repeat;
     background-size: cover;
     background-position: top right;
     color: white;
     text-align: center;
     padding: 1rem;
+  }
 
-    @include flow-y(0.75rem);
+  .hero__heading {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+  .hero__subheading {
+    font-size: 1.55rem;
+    font-weight: normal;
+  }
 
-    &__heading {
-      font-size: 2.5rem;
-      font-weight: bold;
-      @include media("<desktop") {
-        font-size: 2rem;
-      }
+  /** Smaller than desktop*/
+  @media (--lg-max) {
+    .hero__heading {
+      font-size: 2rem;
     }
 
-    &__subheading {
-      font-size: 1.55rem;
-      font-weight: normal;
-      @include media("<desktop") {
-        font-size: 1rem;
-      }
+    .hero__subheading {
+      font-size: 1rem;
     }
   }
 </style>
