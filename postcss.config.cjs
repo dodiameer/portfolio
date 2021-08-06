11888654ed3f35db89af546ffd0bf6005c05e836
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 20:13:15 +0300

    feat: Added PurgeCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,17 +1,25 @@
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const customMedia = require("postcss-custom-media");
+/**
+ * @type {import("@fullhuman/postcss-purgecss").default}
+ */
+// @ts-ignore
+const purgecss = require("@fullhuman/postcss-purgecss")
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
 module.exports = {
 	plugins: [
-		autoprefixer,
+		autoprefixer(),
 		customMedia(),
-		
 		!dev && cssnano({
 			preset: "default",
+		}),
+		!dev && purgecss({
+			content: ["./src/**/*.html", "./src/**/*.svelte"],
+			safelist: [/^svelte-/],
 		}),
 	],
 };
