00a03abd86e4381439a742445894706a508d382d
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 18:18:25 +0300

    refactor: Added button component - site mostly looks good now

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-  import "$lib/styles/global.scss";
+  // import "$lib/styles/global.scss";
   import "$lib/styles/global.css";
   import Navbar from "$lib/components/routes/__layout/Navbar.svelte";
   import BackToTop from "$lib/components/routes/__layout/BackToTop.svelte";
