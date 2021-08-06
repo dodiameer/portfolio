0da44d2130086c92d782fd1b5f185f1939d03d2b
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 18:42:48 +0300

    refactor: Alert component into CSS (non-scoped)

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -6,23 +6,3 @@
 <div class="alert alert-{type}" role="alert">
   <slot />
 </div>
-
-<style lang='scss'>
-  .alert {
-    border: 1px solid transparent;
-    padding: 1rem;
-    border-radius: 0.25rem;
-  }
-
-  .alert-success {
-    border-color: get-color("misc", "success-border");
-    color: get-color("misc", "success-content");
-    background: get-color("misc", "success")
-  }
-
-  .alert-primary {
-    border-color: get-color("primary", "darker");
-    color: get-color("primary", "darkest");
-    background: get-color("primary", "lightest")
-  }
-</style>
