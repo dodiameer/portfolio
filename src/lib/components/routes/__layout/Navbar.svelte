d6f9267f0c9b740122f446937f0cba9ed289012a
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 19:31:12 +0300

    refactor: Navbar into plain CSS

--- src/lib/components/routes/__layout/Navbar.svelte
+++ src/lib/components/routes/__layout/Navbar.svelte
@@ -1,29 +1,39 @@
-<script lang='ts'>
-  import {page} from "$app/stores"
-  let currentRoute: string
+<script lang="ts">
+  import { page } from "$app/stores";
+  let currentRoute: string;
   $: {
-    currentRoute = $page.path.split("/")[1]
+    currentRoute = $page.path.split("/")[1];
   }
 </script>
 
 <template>
   <div class="nav-wrapper">
-      <nav class="nav container" aria-label="Main website navigation">
-        <a href="/home" class="nav__brand" aria-label="dodiameer - link to home page">dodiameer</a>
-        <div class="nav__links">
-          <a href="/home" class="nav__link" class:nav__link--active={currentRoute === "home"}>home</a>
-          <a href="/about" class="nav__link" class:nav__link--active={currentRoute === "about"}>about</a>
-          <a href="/contact" class="nav__link btn">Contact me</a>
-        </div>
-      </nav>
+    <nav class="nav container" aria-label="Main website navigation">
+      <a
+        href="/home"
+        class="nav__brand"
+        aria-label="dodiameer - link to home page">dodiameer</a>
+      <div class="nav__links">
+        <a
+          href="/home"
+          class="nav__link"
+          class:nav__link--active="{currentRoute === 'home'}">home</a>
+        <a
+          href="/about"
+          class="nav__link"
+          class:nav__link--active="{currentRoute === 'about'}">about</a>
+        <a href="/contact" class="nav__link btn">Contact me</a>
+      </div>
+    </nav>
   </div>
 </template>
 
-<style lang='scss'>
+<style lang="postcss">
   .nav-wrapper {
     background: white;
     color: black;
   }
+
   .nav {
     padding-top: 1rem;
     padding-bottom: 1rem;
@@ -31,36 +41,46 @@
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    @include media("<tablet") {
-      @include flow-y;
+  }
+
+  .nav__brand {
+    font-size: 1.5rem;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .nav__links {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .nav__links > * + * {
+    margin-left: 1rem;
+  }
+
+  .nav__link:not(.btn) {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .nav__link--active {
+    color: var(--colors-primary-darker);
+    border-bottom: 1px solid var(--colors-primary-darker);
+  }
+
+  .nav__link.btn {
+    font-size: 0.85rem;
+  }
+
+  /** Mobile styles */
+
+  @media (--md-max) {
+    .nav {
       flex-direction: column;
     }
-    
-    &__brand {
-      font-size: 1.5rem;
-      color: inherit;
-      text-decoration: none;
-    }
-
-    &__links {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      @include flow-x(1rem);
-    }
-    
-    &__link {
-      color: inherit;
-      text-decoration: none;
-      
-      
-      &--active {
-        color: get-color("primary", "darker");
-        border-bottom: 1px solid get-color("primary", "darker")
-      }
-      &.btn {
-        font-size: 0.85rem;
-      }
+    .nav > * + * {
+      margin-top: 0.66rem;
     }
   }
 </style>
