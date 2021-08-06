ac31c364841736a88ab857172a3622cde72ac19b
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-06 19:42:11 +0300

    refactor: Home navbar into plain CSS

--- src/lib/components/routes/home/HomeNavbar.svelte
+++ src/lib/components/routes/home/HomeNavbar.svelte
@@ -1,41 +1,36 @@
 <template>
-  <nav class="nav-wrapper" aria-label="Home page quick navigation">
+  <nav class="nav-wrapper flow-y" aria-label="Home page quick navigation">
     <h2 class="nav-wrapper__title">Quick navigation</h2>
-    <div class="nav container">
+    <div class="nav container flow-x">
       <a href="#skills-section" class="nav__link btn btn-secondary">Skills</a>
       <a href="#work-section" class="nav__link btn btn-secondary">Work</a>
     </div>
   </nav>
 </template>
 
-<style lang='scss'>
+<style lang="postcss">
   .nav-wrapper {
-    @include flow-y(0.66rem);
     padding: 1rem 0.66rem;
-    background: get-color("primary");
-    color: get-color("primary", "content");
-    
-    &__title {
-      font-size: 1.2rem;
-      font-weight: normal;
-      text-align: center;
-      color: inherit;
-    } 
-  } 
+    background: var(--colors-primary-normal);
+    color: var(--colors-primary-content);
+  }
+
+  .nav-wrapper__title {
+    font-size: 1.2rem;
+    font-weight: normal;
+    text-align: center;
+    color: inherit;
+  }
+
   .nav {
     display: flex;
     align-items: center;
     justify-content: center;
-    @include flow-x;
+  }
 
-    &__link {
-      // font-size: 0.85rem;
-      // padding-top: 0.5rem;
-      // padding-bottom: 0.5rem;
-      text-decoration: none;
-      min-width: 10ch;
-      text-align: center;
-      // font-weight: bold
-    }
-  }  
+  .nav__link {
+    text-decoration: none;
+    min-width: 10ch;
+    text-align: center;
+  }
 </style>
