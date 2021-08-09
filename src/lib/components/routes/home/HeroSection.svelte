fe3b38e9e42e6ce74ed69506bf426f87bbbd0c7e
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-08-09 16:19:14 +0300

    fix: Make hero CTA go to contact page

--- src/lib/components/routes/home/HeroSection.svelte
+++ src/lib/components/routes/home/HeroSection.svelte
@@ -2,7 +2,7 @@
   <section class="hero flow-y" id="hero-section">
     <h1 class="hero__heading">I build attractive sites that help you sell</h1>
     <h2 class="hero__subheading">Frontend Developer in İstanbul</h2>
-    <button class="btn">Get in touch</button>
+    <a href="/contact" class="btn">Get in touch</a>
   </section>
 </template>
 
